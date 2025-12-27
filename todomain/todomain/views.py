b06dfac92774dacd5083b2a1403f2326a07eea47
Francis <frankmk2025@gmail.com>
2025-12-27 11:02:21 +0300

    first commit

---
+++ todomain/todomain/views.py
@@ -0,0 +1,9 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+from todo.models import Task
+def home(request):
+    tasks= Task.objects.filter(is_completed=False)
+    context = {
+        'tasks' : tasks,
+    }
+    return render(request, 'home.html', context)
